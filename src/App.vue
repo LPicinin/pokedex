84a5e56d2b799871ae6c6986943b874761ddba9a
Luís Henrique Picinin Jandre <luishpj@outlook.com.br>
2021-08-11 22:13:02 -0300

    inicio e conficuração do projeto Pokedex

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="pokedex">
+    <h1 class="text--yellow">Pokedex DIO</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style  scoped>
+#pokedex {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  margin: 0 auto;
+}
+</style>
